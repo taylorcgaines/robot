fbe8b9bf5bdc86d41c56627a32432859cfed4a76
Taylor Gaines <taylorcgaines@gmail.com>
2017-06-21 13:38:54 -0500

    ALRIGHT THIS IS WHAT I GOT

--- robot-app.js
+++ robot-app.js
@@ -7,6 +7,7 @@ const app = express();
 app.engine('mustache', mustacheExpress());
 app.set('views', './views')
 app.set('view engine', 'mustache')
+app.use(express.static(__dirname + '/public'));
 //Listening on root
 app.get('/', function (req, res) {
   // accessing the users inside the data.js file
@@ -15,6 +16,14 @@ app.get('/', function (req, res) {
       users: data.users
     })
 })
+app.get('/:id', function (req, res) {
+  // accessing the users inside the data.js file
+  var id = req.params.id
+    res.render("id", {
+      users: data.users[id]
+    })
+})
+
 app.listen(3000, function () {
   console.log('Successfully started express application!');
 })
